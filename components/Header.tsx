bef313296d7d7060886966b292b52fcf7dfac510
kamsi19 <kingsleysaba@gmail.com>
2023-11-05 23:46:28 +0000

    First push

---
+++ components/Header.tsx
@@ -0,0 +1,25 @@
+import DarkModeToggle from "./DarkModeToggle";
+import Logo from "./logo";
+
+function Header() {
+  return ( 
+    <header className="sticky top-0 z-50 bg-white dark:bg-grey-900">
+        <nav className="flex flex-col sm:flex-row items-center p-5 pl-2 bg-white dark:bg-grey-900 max-w-7xl mx-auto">
+            <Logo />
+
+            <div className="flex-1 flex items-center justify-end space-x-4">
+                {/* Language select */}
+                {/*  Session && {}
+                    ....
+                */}
+                <DarkModeToggle />
+                {/* DarkmodeToggle */}
+            </div>
+        </nav>
+
+        {/* Upgrade banner */}
+    </header>
+  );
+}
+
+export default Header
